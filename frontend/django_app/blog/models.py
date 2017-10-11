c3b04e0b6cfad10d2992a203802251d5b2211c17
hongdonghyun <hong4367@gmail.com>
2017-10-11 10:59:23 +0900

    블로그 모델 생성 포스트 댓글

--- frontend/django_app/blog/models.py
+++ frontend/django_app/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
