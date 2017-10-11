ca010493b75fd667b94bc3c5c340dfc3e2d31b6b
hongdonghyun <hong4367@gmail.com>
2017-10-11 13:54:21 +0900

    blog post model CRUD 만들었음

--- frontend/django_app/blog/models.py
+++ frontend/django_app/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -6,6 +7,9 @@ class Post(models.Model):
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.pk])
 
 
 class Comment(models.Model):
